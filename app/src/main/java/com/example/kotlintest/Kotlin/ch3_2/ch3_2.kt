a1338a8ccb2593b196ad75ac8f4f691a098c8738
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-10-18 13:26:21 +0900

    [2024-10-17] ch3 Add

--- app/src/main/java/com/example/kotlintest/Kotlin/ch3_2/ch3_2.kt
+++ app/src/main/java/com/example/kotlintest/Kotlin/ch3_2/ch3_2.kt
@@ -30,6 +30,58 @@ fun main(){
 
     val name = "Son"
     println("name : $name, sum : ${sum(10)}, plus : ${10 + 20}")
+
+
+    // 컬렉션 타입 : 여러 개의 데이터를 표현하는 방법 Array List Set Map이 있음
+
+    // Array
+    val arr: Array<Int> = Array(3,{0}) // 배열 생성자는 배열 크기와 초깃값을 매개값으로 받음
+    arr[0] = 10
+    arr[1] = 20
+    arr.set(2,30)
+    println("""
+        array size : ${arr.size}
+        array data : ${arr[0]}, ${arr[1]}, ${arr.get(2)}
+    """.trimIndent())
+    // arrayOf로 선언과 동시에 값을 할당
+    val arr2: Array<Int> = arrayOf(10,20,30)
+    println("""
+        array2 size : ${arr2.size}
+        array2 data : ${arr2[0]}, ${arr2[1]}, ${arr2.get(2)}
+    """.trimIndent())
+    // 기초타입배열
+    val arrInt: IntArray = intArrayOf(10,20,30)
+    val arrBool: BooleanArray = booleanArrayOf(true,false,false)
+
+    // 컬렉션 타입 클래스
+    // list 불변
+    val list: List<Int> = listOf(10,20,30) // listOf 메소드로 리스트 객체 생성
+    println("""
+        list size : ${list.size}
+        list data : ${list[0]}, ${list[1]}, ${list.get(2)}
+    """.trimIndent())
+
+    var mutableList: MutableList<Int> = mutableListOf(10,20,30) // mutableListOf 메소드로 가변 리스트 객체 생성
+    mutableList.add(3,40)
+    mutableList.add(0,60) // 들어있는 인덱스에 값을 넣으면 원래 있던 값들이 뒤로 밀려남 set을 해야 그 인덱스 값이 바뀜
+    println("""
+        mutableList size : ${mutableList.size}
+        mutableList data : ${mutableList[0]}, ${mutableList[1]}, ${mutableList.get(2)}, ${mutableList.get(3)}, ${mutableList.get(4)}
+       
+    """.trimIndent())
+
+    val map: Map<String,String> = mapOf(Pair("one","hello"), "two" to "world")
+    println("""
+        map size : ${map.size}
+        map data : ${map.get("one")}, ${map.get("two")}
+    """.trimIndent())
+
+    var mutableMap: MutableMap<String,String> = mutableMapOf(Pair("three", "name"), "four" to "Son")
+    mutableMap.set("Exit","종료")
+    println("""
+        mutableMap : size = ${mutableMap.size}
+        mutableMap : data = ${mutableMap.get("three")}, ${mutableMap.get("four")}, ${mutableMap.get("Exit")}
+    """.trimIndent())
 }
 
 fun some(): Unit { // 반환문이 없는 함수 fun some(){}와 같음
